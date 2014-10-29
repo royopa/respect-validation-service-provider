0b3397f32ce998a5ac31afb83b1601f6c38fa5fe
royopa <royopa@gmail.com>
2014-10-28 23:11:00 -0200

    corrected test

---
+++ tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+ * This file is part of the RespectValidationerviceProvider package.
+ *
+ * (c) Rodrigo Prado de Jesus <royopa@gmail.com>
+ *
+ */
+
+$loader = require __DIR__.'/../../../../vendor/autoload.php';
